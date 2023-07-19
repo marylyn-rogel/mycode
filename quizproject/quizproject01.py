7be2da1389f3485480d688031dcc575dbe28f941
marylyn-rogel <marylyn.rogel1@egmail.com>
2023-07-19 21:11:40 +0000

    While looping

--- quizproject/quizproject01.py
+++ quizproject/quizproject01.py
@@ -6,22 +6,27 @@
 #puss in boots, kitty softpaws, or perrito
 
 
+#telling the code to run until it breaks
+while True:
 
 ## Collect input from user
-last_wish = input("What would you wish for, if you had the magic star? (options: More Lives, Someone to Trust, or A Family):  ")
+    last_wish = input("What would you wish for, if you had the magic star? (options: More Lives, Someone to Trust, or A Family):  ")
 
 ## using .lower and .strip to try and prevent input errors
-if last_wish.lower().strip() == "more lives":
-    print("You want more lives. You must be Puss In Boots")
+    if last_wish.lower().strip() == "more lives":
+        print("You want more lives. You must be Puss In Boots")
+        break
 
-elif last_wish.lower().strip() == "someone to trust":
-    print("You want someone to trust. You must be Kitty SoftPaws")
+    elif last_wish.lower().strip() == "someone to trust":
+        print("You want someone to trust. You must be Kitty SoftPaws")
+        break
 
-elif last_wish.lower().strip() == "a family":
-    print("you want a family. you must be perrito")
-
-else: 
-    print("sorry. try again!")
+    elif last_wish.lower().strip() == "a family":
+        print("you want a family. you must be perrito")
+        break
+    
+    else:
+        print("sorry that is not an option. try again and choose one of the following: More Lives, Someone to Trust, or A Family!")
 
 
 
